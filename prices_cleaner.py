7f1241f48770a340fdb786d27d5565ef246a7f08
Riccardo Felici <riccardofelici7@gmail.com>
2024-05-20 09:40:48 +0200

    Initial commit

---
+++ prices_cleaner.py
@@ -0,0 +1,23 @@
+import pandas as pd
+df = pd.read_csv('historical_stock_prices.csv')
+
+# Rimuovere valori nulli o mancanti
+df = df.dropna()
+
+# Filtra i valori con prezzi negativi e con volumi di scambio negativi
+df = df[df['close'] > 0]
+df = df[df['volume'] > 0]
+
+# gestione outliers
+from scipy import stats
+import numpy as np
+df = df[(np.abs(stats.zscore(df['close'])) < 3)]
+
+# Rimuove righe con date fuori intervallo
+df['date'] = pd.to_datetime(df['date'])
+df = df[(df['date'] >= '1970-01-01') & (df['date'] <= '2018-12-31')]
+
+# Rimuove duplicati
+df = df.drop_duplicates()
+
+df.to_csv('historical_stock_prices_pulito.csv', index=False)
