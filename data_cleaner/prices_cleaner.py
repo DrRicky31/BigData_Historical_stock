26595637eb4c4d5e113db9446b847ccfd9002227
Riccardo Felici <riccardofelici7@gmail.com>
2024-05-22 16:53:32 +0200

    Update prices_cleaner.py

--- data_cleaner/prices_cleaner.py
+++ data_cleaner/prices_cleaner.py
@@ -13,11 +13,19 @@ from scipy import stats
 import numpy as np
 df = df[(np.abs(stats.zscore(df['close'])) < 3)]
 
+# Rimuove duplicati
+df = df.drop_duplicates()
+
 # Rimuove righe con date fuori intervallo
 df['date'] = pd.to_datetime(df['date'])
 df = df[(df['date'] >= '1970-01-01') & (df['date'] <= '2018-12-31')]
 
-# Rimuove duplicati
-df = df.drop_duplicates()
+# Calcola le date minime e massime per ogni anno
+first_dates = df.groupby(df['date'].dt.year)['date'].min()
+last_dates = df.groupby(df['date'].dt.year)['date'].max()
 
+# Filtra il DataFrame originale
+df = df[df['date'].isin(first_dates) | df['date'].isin(last_dates)]
+
+# Salva il DataFrame pulito in un nuovo file CSV
 df.to_csv('historical_stock_prices_pulito.csv', index=False)
