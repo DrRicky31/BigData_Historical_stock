95cd4dec54d411ecbf83e63a5cd48c9e3f4244ed
Riccardo Felici <riccardofelici7@gmail.com>
2024-05-23 09:14:18 +0200

    bug fix for 1-MapReduce

---
+++ 1-MapReduce/1-data_cleaner/prices_cleaner.py
@@ -0,0 +1,24 @@
+import pandas as pd
+df = pd.read_csv('historical_stock_prices.csv')
+
+# Rimuovere valori nulli o mancanti
+df = df.dropna()
+
+# Filtra i valori con prezzi negativi e con volumi di scambio negativi
+df = df[df['close'] > 0]
+df = df[df['volume'] > 0]
+
+# gestione outliers
+from scipy import stats
+import numpy as np
+df = df[(np.abs(stats.zscore(df['close'])) < 3)]
+
+# Rimuove duplicati
+df = df.drop_duplicates()
+
+# Rimuove righe con date fuori intervallo
+df['date'] = pd.to_datetime(df['date'])
+df = df[(df['date'] >= '1970-01-01') & (df['date'] <= '2018-12-31')]
+
+# Salva il DataFrame pulito in un nuovo file CSV
+df.to_csv('historical_stock_prices_pulito.csv', index=False)
