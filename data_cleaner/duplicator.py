a284d48d1e51212e64a062f8727371d57c4b0441
alemari7 <ale.mari0803@gmail.com>
2024-06-05 08:09:23 +0200

    update duplicator.py

--- data_cleaner/duplicator.py
+++ data_cleaner/duplicator.py
@@ -2,8 +2,8 @@ import random
 import pandas as pd
 
 def modify_value(value):
-    if isinstance(value, int):
-        return value * random()
+    if isinstance(value, (int, float)):
+        return value * random.random()
     else:
         return value
 
@@ -15,9 +15,9 @@ def process_csv(file_path):
     df_doubled = pd.concat([df, df], ignore_index=True)
     
     # Modifica i valori nel DataFrame raddoppiato
-    df_modified = df_doubled.applymap(modify_value)
+    df_modified = df_doubled.map(modify_value)
     
-   # Salva il DataFrame modificato in un nuovo file CSV
+    # Salva il DataFrame modificato in un nuovo file CSV
     output_file_path = file_path.replace("_pulito.csv", "_double.csv")
     df_modified.to_csv(output_file_path, index=False)
     print(f"File salvato come {output_file_path}")
